3feccd50eb1ba17532305b3da3329b456b860456
sebastian.coceres <sebastian.coceres11@gmail.com>
2026-08-12 18:18:20 +0200

    feat: update SlideTitle component to enhance accessibility messaging

--- src/components/slides/SlideTitle.jsx
+++ src/components/slides/SlideTitle.jsx
@@ -2,9 +2,8 @@ import { Slide } from '@revealjs/react';
 
 export default function SlideTitle() {
   return (
-    <Slide>
-      <h1 className="text-5xl font-bold text-blue-400 mb-6">Accesibilidad llevada a la práctica</h1>
-      <h3 className="text-3xl text-gray-300">Diseñar para que TODAS las personas puedan usar el producto</h3>
+    <Slide >
+      <p className="text-5xl text-gray-300 text-balance">Diseñar para que TODAS las personas puedan usar el producto</p>
     </Slide>
   );
 }
